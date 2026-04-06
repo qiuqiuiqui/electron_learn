4a45669dd03b80173295e403a4fda2598e774306
邱林康 <11187856+qiulinkang@user.noreply.gitee.com>
2026-04-06 11:15:43 +0800

    第二版本

--- forge.config.js
+++ forge.config.js
@@ -5,5 +5,18 @@ module.exports = {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'win32', 'linux']
     }
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'qiuqiuiqui',
+          name: 'electron_learn'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
   ]
 };
